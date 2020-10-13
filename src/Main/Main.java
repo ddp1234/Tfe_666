adda933e9c78030927079c2c0547af669879952f
ddp <1178766740@qq.com>
2020-10-14 07:59:27 +0800

    ddp

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,10 +15,10 @@ public class Main{//分数
 public static void main(String[] args)  
 {
     int []score ={67,98,75,63,82,79,81,91,66,84};
-
+System.out.println("***排序结果是*****");
     Sort sort = new Sort();
     sort.sort(score);
-    System.out.println("-------------------------------");
+    System.out.println("--------------平均值-----------------");
     Mmq mmq = new Mmq();
    mmq.getAvgScore(score);
    System.out.println("最大值");
